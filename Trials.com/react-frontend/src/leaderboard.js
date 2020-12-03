2becf183a1d169ee21d3b216276a541e1d6dfb6b
Randall (RJ) Bradach <49658348+rjburgerr1@users.noreply.github.com>
2020-12-03 06:26:15 -0600

    Remade core react-app with better setup
    Made new react-app and transposed all current files to it for a fresh start.

---
+++ Trials.com/react-frontend/src/leaderboard.js
@@ -0,0 +1,95 @@
+export const columns = [
+  {
+    Header: "First Name",
+    accessor: "firstName",
+  },
+  {
+    Header: "Last Name",
+    accessor: "lastName",
+  },
+  {
+    Header: "Age",
+    accessor: "age",
+  },
+  {
+    Header: "Gender",
+    accessor: "gender",
+  },
+  {
+    Header: "Grade",
+    accessor: "grade",
+  },
+];
+
+export const data = [
+  {
+    firstName: "Alice",
+    lastName: "Johnson",
+    age: 9,
+    gender: "F",
+    grade: 4,
+  },
+  {
+    firstName: "Mike",
+    lastName: "Ford",
+    age: 5,
+    gender: "M",
+    grade: 1,
+  },
+  {
+    firstName: "John",
+    lastName: "Smith",
+    age: 8,
+    gender: "M",
+    grade: 4,
+  },
+  {
+    firstName: "Joe",
+    lastName: "Johnson",
+    age: 11,
+    gender: "M",
+    grade: 6,
+  },
+  {
+    firstName: "Linda",
+    lastName: "Ford",
+    age: 8,
+    gender: "F",
+    grade: 5,
+  },
+  {
+    firstName: "Noah",
+    lastName: "Wilson",
+    age: 9,
+    gender: "M",
+    grade: 3,
+  },
+  {
+    firstName: "Emma",
+    lastName: "Lee",
+    age: 7,
+    gender: "F",
+    grade: 3,
+  },
+  {
+    firstName: "James",
+    lastName: "Jones",
+    age: 10,
+    gender: "M",
+    grade: 5,
+  },
+  {
+    firstName: "Mia",
+    lastName: "Brown",
+    age: 8,
+    gender: "F",
+    grade: 5,
+  },
+  {
+    firstName: "William",
+    lastName: "Davis",
+    age: 11,
+    gender: "M",
+    grade: 6,
+  },
+];

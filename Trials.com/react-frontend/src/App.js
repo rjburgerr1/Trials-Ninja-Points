44f6ccba852e9f134d4af82f004e82e0865b1c2c
Randall (RJ) Bradach <49658348+rjburgerr1@users.noreply.github.com>
2020-12-11 11:55:10 -0600

    Add firebase authentication setup

--- Trials.com/react-frontend/src/App.js
+++ Trials.com/react-frontend/src/App.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import SignInSignUp from './pages/signin_signup';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import SignInSignUp from "./pages/signin_signup";
+import { Container } from "react-bootstrap";
+import { AuthProvider } from "./contexts/AuthContext";
 
 function App() {
-    return (
-        <Router>
-            <Switch>
-                <Route path='/' exact component={SignInSignUp} />
-            </Switch>
-        </Router>
-    );
+  return (
+    <AuthProvider>
+      <Container>
+        <SignInSignUp />
+      </Container>
+    </AuthProvider>
+  );
 }
 
 export default App;
--- Trials.com/react-frontend/src/pages/signin_signup.js
+++ Trials.com/react-frontend/src/pages/signin_signup.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./css/signin_signup.css";
+import { useAuth } from "../contexts/AuthContext";
 
+export default function Signin_Signup() {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const { signup } = useAuth();
+  const [error, setError] = useState("");
 
-export default function signin_signup() {
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    try {
+      signup(emailRef.current.value, passwordRef.current.value);
+    }
+  }
+
   const SignUpTransition = () => {
     const container = document.getElementById("container");
     container.classList.add("right-panel-active");
@@ -17,46 +30,62 @@ export default function signin_signup() {
   return (
     <div class="signin_signup">
       <div class="container" id="container">
-
         <div class="form-container sign-up-container">
           <form action="http://localhost:3002/sign-up-complete" method="POST">
             <h1>Create Account</h1>
 
             <div class="social-container"></div>
 
-            <input type="text" placeholder="Username" name="username"/>
-            <input type="email" placeholder="Email" name="email"/>
-            <input type="password" placeholder="Password" name="password"/>
+            <input type="text" placeholder="Username" name="username" />
+            <input
+              type="email"
+              placeholder="Email"
+              name="email"
+              ref={emailRef}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              ref={passwordRef}
+            />
             <button type="submit">Sign Up</button>
           </form>
         </div>
 
         <div class="form-container sign-in-container">
-            <form action="http://localhost:3002/users" method="POST">
-              <h1>Sign in</h1>
-            
-              <div class="social-container"></div>
+          <form action="http://localhost:3002/users" method="POST">
+            <h1>Sign in</h1>
 
-              <input type="email" placeholder="Email"/>
-              <input type="password" placeholder="Password"/>
-              <a href="#">Forgot your password?</a>
-              <button>Sign In</button>
-            </form>
-          </div>
-        
+            <div class="social-container"></div>
+
+            <input type="email" placeholder="Email" required />
+            <input type="password" placeholder="Password" required />
+            <a href="\#">Forgot your password?</a>
+            <button>Sign In</button>
+          </form>
+        </div>
+
         <div class="overlay-container">
           <div class="overlay">
-
             <div class="overlay-panel overlay-left">
               <h1>Welcome back!</h1>
-              <p>To keep connected with us please login with your personal info</p>
-              <button class="ghost" id="signIn" onClick={SignInTransition}>Sign In</button>
+              <p>
+                To keep connected with us please login with your personal info
+              </p>
+              <button class="ghost" id="signIn" onClick={SignInTransition}>
+                Sign In
+              </button>
             </div>
 
             <div class="overlay-panel overlay-right">
               <h1>Hi, gamer!</h1>
-              <p>Enter your personal details and start your ninja journey with us</p>
-              <button class="ghost" id="signUp" onClick={SignUpTransition}>Sign Up</button>
+              <p>
+                Enter your personal details and start your ninja journey with us
+              </p>
+              <button class="ghost" id="signUp" onClick={SignUpTransition}>
+                Sign Up
+              </button>
             </div>
           </div>
         </div>

3f16fb3b7b9250c24edc2652ede67a72fb4adc6b
Randall (RJ) Bradach <49658348+rjburgerr1@users.noreply.github.com>
2020-12-11 12:00:33 -0600

    Refactor index.js and App.js to redirect to signin page correctly

--- Trials.com/react-frontend/src/App.js
+++ Trials.com/react-frontend/src/App.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import SignInSignUp from './pages/signin_signup';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import SignInSignUp from "./pages/signin_signup";
 
 function App() {
-    return (
-        <Router>
-            <Switch>
-                <Route path='/' exact component={SignInSignUp} />
-            </Switch>
-        </Router>
-    );
+  return (
+    <Router>
+      <Switch>
+        <Route path="/rjburgerr1.github.io" exact component={SignInSignUp} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;

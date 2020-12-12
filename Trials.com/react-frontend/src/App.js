a666aee28add49af9ee98d6f1ed9e7d62236499c
Randall (RJ) Bradach <49658348+rjburgerr1@users.noreply.github.com>
2020-12-12 17:45:39 -0600

    Add dashboard for profile and redirects
    Redirect Users from log in to profile and back with log out functionality

--- Trials.com/react-frontend/src/App.js
+++ Trials.com/react-frontend/src/App.js
@@ -3,14 +3,19 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import SignInSignUp from "./pages/signin_signup";
 import { AuthProvider } from "./contexts/AuthContext";
 import Dashboard from "./pages/dashboard";
+import PrivateRoute from "./components/privateRoute.component";
+import ResetPassword from "./components/resetPassword.component";
+import UpdateProfile from "./components/updateProfile.component";
 
 function App() {
   return (
     <Router>
       <AuthProvider>
         <Switch>
-          <Route exact path="/" component={Dashboard} />
+          <PrivateRoute exact path="/" component={Dashboard} />
           <Route path="/signin" component={SignInSignUp} />
+          <Route path="/forgot-password" component={ResetPassword} />
+          <Route path="/update-profile" component={UpdateProfile} />
         </Switch>
       </AuthProvider>
     </Router>

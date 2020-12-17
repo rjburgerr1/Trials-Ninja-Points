b7f1343900d54f47e6f5db99e1fcc5959c143211
Randall (RJ) Bradach <49658348+rjburgerr1@users.noreply.github.com>
2020-12-16 22:28:41 -0600

    Delete forgot-password.js

--- Trials.com/react-frontend/src/components/forgot-password.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function ForgotPassword() {
-  return (
-    <div class="Forgot Password">
-      <button type="submit">Forgot Password?</button>
-    </div>
-  );
-}

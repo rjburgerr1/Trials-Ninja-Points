364cca111042fc2fa7ab97b6eb5d46afc31d6cf3
Randall (RJ) Bradach <49658348+rjburgerr1@users.noreply.github.com>
2020-12-16 22:30:37 -0600

    Fix merge conflicts

--- Trials.com/react-frontend/src/components/login.js
+++ Trials.com/react-frontend/src/components/login.js
@@ -1,5 +1,5 @@
 import React, { useRef, useState } from "react";
-import { useAuth } from "../contexts/AuthContext";
+import { useAuth } from "../contexts/auth-context";
 import { Alert, Button } from "react-bootstrap";
 import { Link, useHistory } from "react-router-dom";
 

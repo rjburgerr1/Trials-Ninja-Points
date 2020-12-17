8b1f55da51b907fb750b1cbd54e79488a9c075d6
Randall (RJ) Bradach <49658348+rjburgerr1@users.noreply.github.com>
2020-12-16 21:50:46 -0600

    Add main leaderboard to start

--- Trials.com/react-frontend/src/components/logout.component.jsx
+++ Trials.com/react-frontend/src/components/logout.component.jsx
@@ -4,7 +4,7 @@ import { Button } from "react-bootstrap";
 import { useHistory } from "react-router-dom";
 
 export default function Logout() {
-  const [setError] = useState("");
+  const [error, setError] = useState("");
   const { logout } = useAuth();
   const history = useHistory();
 

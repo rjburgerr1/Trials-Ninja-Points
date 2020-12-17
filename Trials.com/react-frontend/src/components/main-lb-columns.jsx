3c02d1802227ae2073325636278934fe6ce9b899
Randall (RJ) Bradach <49658348+rjburgerr1@users.noreply.github.com>
2020-12-16 20:07:28 -0600

    Add react-table leaderboard for main leaderboard

---
+++ Trials.com/react-frontend/src/components/main-lb-columns.jsx
@@ -0,0 +1,22 @@
+export const COLUMNS = [
+  {
+    Header: "Rank",
+    accessor: "Rank",
+  },
+  {
+    Header: "Rider Name",
+    accessor: "Rider Name",
+  },
+  {
+    Header: "Ninja Points",
+    accessor: "Ninja-Points",
+  },
+  {
+    Header: "# Runs",
+    accessor: "# Runs",
+  },
+  {
+    Header: "Best NP Run",
+    accessor: "Best NP Run",
+  },
+];

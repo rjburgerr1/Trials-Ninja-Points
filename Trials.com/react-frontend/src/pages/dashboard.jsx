5d2023fb74d0b31d8b58e152d7281a7f08c0075a
Randall (RJ) Bradach <49658348+rjburgerr1@users.noreply.github.com>
2020-12-16 20:07:28 -0600

    Add react-table leaderboard for main leaderboard

--- Trials.com/react-frontend/src/pages/dashboard.jsx
+++ Trials.com/react-frontend/src/pages/dashboard.jsx
@@ -3,7 +3,13 @@ import { Card, Button, Alert } from "react-bootstrap";
 import { useAuth } from "../contexts/AuthContext";
 import { Link, useHistory } from "react-router-dom";
 import NavBar from "../components/navbar.component";
+import { MainLB } from "../components/main-leaderboard.component";
 
 export default function Dashboard() {
-  return <NavBar />;
+  return (
+    <div>
+      <NavBar />
+      <MainLB />
+    </div>
+  );
 }

2f64bc3c158104c4b1198eafa5ff4711833f5bad
Randall (RJ) Bradach <49658348+rjburgerr1@users.noreply.github.com>
2020-12-20 20:00:47 -0600

    Add Submit Run Component Frontend
    - Added input fields for submitting track name, creator, faults, and time.
    - Added slider for selecting ninja level
    - Added dropwdowns for length and fault sponginess fields.
    - Prevented fault field to exceed 500

--- Trials.com/react-frontend/src/components/navbar.js
+++ Trials.com/react-frontend/src/components/navbar.js
@@ -13,7 +13,7 @@ export default function NavBar() {
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto">
           <Nav.Link href="/">Home</Nav.Link>
-          <Nav.Link href="#submit-run">Submit-Run</Nav.Link>
+          <Nav.Link href="submit-run">Submit-Run</Nav.Link>
           <NavDropdown alignRight title={<Icon />} id="basic-nav-dropdown">
             <NavDropdown.Item href="#profile">View Profile</NavDropdown.Item>
             <NavDropdown.Item>

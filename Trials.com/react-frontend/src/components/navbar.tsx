373ba12252dcede6445047b26875e36d7a9ea8a0
Randall (RJ) Bradach <49658348+rjburgerr1@users.noreply.github.com>
2021-01-13 03:12:09 -0600

    Add route to each individual users profile
    Add hyperlink to each user on leaderboard

--- Trials.com/react-frontend/src/components/navbar.tsx
+++ Trials.com/react-frontend/src/components/navbar.tsx
@@ -3,6 +3,8 @@ import Icon from "./icon";
 import { Navbar, Nav, NavDropdown } from "react-bootstrap";
 import Logout from "./logout";
 
+import { useAuth } from "../contexts/auth-context";
+
 export default function NavBar() {
   return (
     <Navbar fixed="top" bg="light" expand="lg">
@@ -13,9 +15,9 @@ export default function NavBar() {
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto">
           <Nav.Link href="/">Home</Nav.Link>
-          <Nav.Link href="submit-run">Submit-Run</Nav.Link>
+          <Nav.Link href="/submit-run">Submit-Run</Nav.Link>
           <NavDropdown alignRight title={<Icon />} id="basic-nav-dropdown">
-            <NavDropdown.Item href="profile">View Profile</NavDropdown.Item>
+            <NavDropdown.Item href="profile/">View Profile</NavDropdown.Item>
             <NavDropdown.Item>
               <Logout />
             </NavDropdown.Item>

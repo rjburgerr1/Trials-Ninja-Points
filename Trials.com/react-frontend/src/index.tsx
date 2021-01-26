8bd7ba57078861fec00d2996d929553af56991b7
Randall (RJ) Bradach <49658348+rjburgerr1@users.noreply.github.com>
2021-01-26 04:12:33 -0600

    Add development and production proxy to node server
    - server.js represents the node server that is running on a separate domain, its not actually being used here. Only to show the current state of the server on the other domain
    - Change from react proxy in package,json to axios baseUrl and added CORS policy within the server.js file

--- Trials.com/react-frontend/src/index.tsx
+++ Trials.com/react-frontend/src/index.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import axios from "axios";
 
+axios.defaults.baseURL = "http://66.42.118.153/api";
 ReactDOM.render(<App />, document.getElementById("root"));

41879cc2bf7ddd651c952a42a33c73f6ddd5afa7
Randall (RJ) Bradach <49658348+rjburgerr1@users.noreply.github.com>
2021-02-10 03:39:56 -0600

    Add runs to main app file

--- Trials.com/react-frontend/src/App.tsx
+++ Trials.com/react-frontend/src/App.tsx
@@ -7,6 +7,7 @@ import PrivateRoute from "./components/private-route";
 import ResetPassword from "./components/reset-password";
 import UpdateProfile from "./components/update-profile";
 import SubmitRun from "./components/submit-run";
+import { Runs } from "./components/leaderboards/runs-leaderboard";
 import Profile from "./pages/profile";
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
           <PrivateRoute exact path="/" component={Dashboard} />
           <Route path="/signin" component={SignInSignUp} />
           <Route path="/forgot-password" component={ResetPassword} />
+          <Route path="/runs" component={Runs} />
           <PrivateRoute path="/update-profile" component={UpdateProfile} />
           <PrivateRoute path="/submit-run" component={SubmitRun} />
           <PrivateRoute path="/profile/:user?" component={Profile} />

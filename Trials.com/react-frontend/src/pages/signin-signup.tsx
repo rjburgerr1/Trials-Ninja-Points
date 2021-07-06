b579419939e91269d24926f555a49a05b20cb3fc
Randall (RJ) Bradach <49658348+rjburgerr1@users.noreply.github.com>
2021-07-06 05:30:56 -0500

    Encapsulate signup styling with div to prevent styles bleeding into other places

--- Trials.com/react-frontend/src/pages/signin-signup.tsx
+++ Trials.com/react-frontend/src/pages/signin-signup.tsx
@@ -22,29 +22,31 @@ const SigninSignup: React.FC<SigninSignupProps> = () => {
 	return (
 		//  it'd be nice if we could end up changing this outside div to Card to be consistent, but it breaks right now
 		//  if not, thats fine, since this is a special case we will probably only use once
-		<div className="container signin-signup" id="container">
-			<Signup />
-			<Login />
-			<div className="overlay-container">
-				<div className="overlay">
-					<div className="overlay-panel overlay-left">
-						<h1>Welcome back!</h1>
-						<p>
-							To keep connected with us please login with your personal info
-						</p>
-						<button className="ghost" id="signIn" onClick={SignInTransition}>
-							Sign In
-						</button>
-					</div>
+		<div className="signin-signup-outer">
+			<div className="container signin-signup" id="container">
+				<Signup />
+				<Login />
+				<div className="overlay-container">
+					<div className="overlay">
+						<div className="overlay-panel overlay-left">
+							<h1>Welcome back!</h1>
+							<p>
+								To keep connected with us please login with your personal info
+							</p>
+							<button className="ghost" id="signIn" onClick={SignInTransition}>
+								Sign In
+							</button>
+						</div>
 
-					<div className="overlay-panel overlay-right">
-						<h1>Hi, gamer!</h1>
-						<p>
-							Enter your personal details and start your ninja journey with us
-						</p>
-						<button className="ghost" id="signUp" onClick={SignUpTransition}>
-							Sign Up
-						</button>
+						<div className="overlay-panel overlay-right">
+							<h1>Hi, gamer!</h1>
+							<p>
+								Enter your personal details and start your ninja journey with us
+							</p>
+							<button className="ghost" id="signUp" onClick={SignUpTransition}>
+								Sign Up
+							</button>
+						</div>
 					</div>
 				</div>
 			</div>

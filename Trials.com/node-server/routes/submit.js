78199b233b58d825050d28dd0d153320944a5379
Randall (RJ) Bradach <49658348+rjburgerr1@users.noreply.github.com>
2021-07-06 01:26:46 -0500

    Add error message to API request

--- Trials.com/node-server/routes/submit.js
+++ Trials.com/node-server/routes/submit.js
@@ -3,35 +3,36 @@ const prisma = new PrismaClient();
 // const { request } = require("express"); Keep for now, might need later
 
 const router = (app) => {
-  // Add a new run
-  // ` (grave) for column and ' (apostrophe) for values for syntax fun :)
-  //Name attribute in form element sets "'gamertag', and 'rank' etc...." name = ninjaLevel === request.body.ninjaLevel
-  //Format:
-  //sql: "INSERT INTO runs SET `gamertag` = 'efwef', `rank` = 'wefwef', `faults` = 'wefwe', `time` = 'fwefwef', `track-name` = 'wefwef', `ninja-points` = 'wefwef'"
+	// Add a new run
+	// ` (grave) for column and ' (apostrophe) for values for syntax fun :)
+	//Name attribute in form element sets "'gamertag', and 'rank' etc...." name = ninjaLevel === request.body.ninjaLevel
+	//Format:
+	//sql: "INSERT INTO runs SET `gamertag` = 'efwef', `rank` = 'wefwef', `faults` = 'wefwe', `time` = 'fwefwef', `track-name` = 'wefwef', `ninja-points` = 'wefwef'"
 
-  app.post("/submitted-run", async (request, response) => {
-    // For some reason that I cannot seem to figure out. request.body returns an object with key value pairs from the submit form
-    // and all of the values are strings even if they were not strings in the form
+	app.post("/submitted-run", async (request, response) => {
+		// For some reason that I cannot seem to figure out. request.body returns an object with key value pairs from the submit form
+		// and all of the values are strings even if they were not strings in the form
 
-    // Convert specific fields to number, before insertion in db
-    request.body.faults = Number(request.body.faults);
-    request.body.rank = Number(request.body.rank);
-    request.body.ninjaLevel = Number(request.body.ninjaLevel);
-    request.body.rating = Number(request.body.rating);
-    request.body.ninjaPoints = Number(request.body.ninjaPoints);
+		// Convert specific fields to number, before insertion in db
+		request.body.faults = Number(request.body.faults);
+		request.body.rank = Number(request.body.rank);
+		request.body.ninjaLevel = Number(request.body.ninjaLevel);
+		request.body.rating = Number(request.body.rating);
+		request.body.ninjaPoints = Number(request.body.ninjaPoints);
 
-    const run = { ...request.body };
-    delete run.creator; // creator does not belong in the run table, so remove it before inserting values
+		const run = { ...request.body };
+		delete run.creator; // creator does not belong in the run table, so remove it before inserting values
 
-    try {
-      await prisma.runs.create({
-        data: { ...run }, // map run fields over data property
-      });
-      return response.sendStatus(200);
-    } catch (error) {
-      return response.status(400).send("BAD REQUEST");
-    }
-  });
+		try {
+			await prisma.runs.create({
+				data: { ...run }, // map run fields over data property
+			});
+			return response.sendStatus(200);
+		} catch (error) {
+			console.log(error);
+			return response.status(400).send("BAD REQUEST");
+		}
+	});
 };
 // Export the router
 module.exports = router;

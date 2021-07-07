23da26ebb9ec7b3974d9405575bbe669c858296b
Randall (RJ) Bradach <49658348+rjburgerr1@users.noreply.github.com>
2021-07-06 21:04:34 -0500

    Change import for prisma client to target new output location

--- Trials.com/node-server/routes/signup.js
+++ Trials.com/node-server/routes/signup.js
@@ -1,17 +1,17 @@
-const { PrismaClient } = require("@prisma/client");
+const { PrismaClient } = require("../../react-frontend/prisma/client");
 const prisma = new PrismaClient();
 
 const router = (app) => {
-  // Add a profile to the databasewhen signup is successful
-  app.post("/sign-up-complete", async (request, response) => {
-    await prisma.profiles.create({
-      data: {
-        email: request.body.email,
-        username: request.body.username,
-        id: request.body.userid,
-      },
+    // Add a profile to the databasewhen signup is successful
+    app.post("/sign-up-complete", async (request, response) => {
+        await prisma.profiles.create({
+            data: {
+                email: request.body.email,
+                username: request.body.username,
+                id: request.body.userid,
+            },
+        });
     });
-  });
 };
 // Export the router
 module.exports = router;

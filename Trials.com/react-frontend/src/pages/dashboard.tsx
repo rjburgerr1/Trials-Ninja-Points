760630c968f68f476d4ed23c36e125fb1ca0b958
Randall (RJ) Bradach <49658348+rjburgerr1@users.noreply.github.com>
2021-07-10 22:48:47 -0500

    Refactor dashboard to include classNames for styling and rename stickytable

--- Trials.com/react-frontend/src/pages/dashboard.tsx
+++ Trials.com/react-frontend/src/pages/dashboard.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import NavBar from "../components/navbar";
-import { StickyTable } from "../components/leaderboards/sticky-table";
+import "../styling/dashboard.scss";
+import { MainLeaderboard } from "../components/leaderboards/main-leaderboard";
 
 export default function Dashboard(props: any) {
     return (
-        <div>
-            <NavBar {...props} />
-            <StickyTable />
+        <div className="dashboard-container">
+            <div className="dashboard-header">
+                <NavBar {...props} />
+            </div>
+
+            <div className="dashboard-body">
+                <MainLeaderboard />
+            </div>
         </div>
     );
 }

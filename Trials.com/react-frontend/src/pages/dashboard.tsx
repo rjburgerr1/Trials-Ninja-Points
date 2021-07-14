e6515cff5cff58e9d39dd7aa4d006ac1f8d5a193
Randall (RJ) Bradach <49658348+rjburgerr1@users.noreply.github.com>
2021-07-14 13:00:42 -0500

    Add chat to dashboard

--- Trials.com/react-frontend/src/pages/dashboard.tsx
+++ Trials.com/react-frontend/src/pages/dashboard.tsx
@@ -1,7 +1,7 @@
-import React from "react";
-import NavBar from "../components/navbar";
 import "../styling/dashboard.scss";
+import { Chat } from "../components/General-Chat/chat";
 import { MainLeaderboard } from "../components/leaderboards/main-leaderboard";
+import NavBar from "../components/navbar";
 
 export default function Dashboard(props: any) {
     return (
@@ -9,9 +9,15 @@ export default function Dashboard(props: any) {
             <div className="dashboard-header">
                 <NavBar {...props} />
             </div>
-
             <div className="dashboard-body">
-                <MainLeaderboard />
+                <div className="dashboard-chat">
+                    <Chat />
+                </div>
+                <div className="dashboard-leaderboard">
+                    <MainLeaderboard />
+                </div>
+
+                <div className="dashboard-space-element"></div>
             </div>
         </div>
     );

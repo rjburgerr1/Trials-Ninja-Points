3a63facfe26e60d971710062d6e52005a1a18149
Randall (RJ) Bradach <49658348+rjburgerr1@users.noreply.github.com>
2021-09-28 20:56:10 -0500

    Fix column header widths and accessors

--- Trials.com/react-frontend/src/components/leaderboards/runs-leaderboard-columns.tsx
+++ Trials.com/react-frontend/src/components/leaderboards/runs-leaderboard-columns.tsx
@@ -1,42 +1,51 @@
 export const COLUMNS = [
-  {
-    Header: "Rider", // Header is the title for the column being displayed (can be anything)
-    accessor: "rider", // accessor is based off of the prisma schemas name mapped for the database column you want
-  },
-  {
-    Header: "Track",
-    accessor: "trackName",
-  },
-  {
-    Header: "Ninja Points",
-    accessor: "ninjaPoints",
-  },
-  {
-    Header: "Ninja Level (Opinion)",
-    accessor: "ninjaLevel",
-  },
-  {
-    Header: "Rank",
-    accessor: "rank",
-  },
-  {
-    Header: "Faults",
-    accessor: "faults",
-  },
-  {
-    Header: "Time",
-    accessor: "time",
-  },
-  {
-    Header: "Length (Opinion)",
-    accessor: "length",
-  },
-  {
-    Header: "Fault Sponginess (Opinion)",
-    accessor: "faultSponginess",
-  },
-  {
-    Header: "Rating (Opinion)",
-    accessor: "rating",
-  },
+    {
+        Header: "Rider", // Header is the title for the column being displayed (can be anything)
+        accessor: "rider", // accessor is based off of the prisma schemas name mapped for the database column you want
+    },
+    {
+        Header: "Track",
+        accessor: "track_name",
+        width: 210,
+    },
+    {
+        Header: "Ninja Points",
+        accessor: "ninja_points",
+        width: 125,
+    },
+    {
+        Header: "Ninja Level",
+        accessor: "ninja_level",
+        width: 125,
+    },
+    {
+        Header: "Rank",
+        accessor: "rank",
+        width: 110,
+    },
+    {
+        Header: "Faults",
+        accessor: "faults",
+        width: 105,
+    },
+    {
+        Header: "Time",
+        accessor: "time",
+        width: 120,
+    },
+    {
+        Header: "Length",
+        accessor: "length",
+        width: 110,
+    },
+    {
+        Header: "Fault Sponginess",
+        accessor: "fault_sponginess",
+        width: 165,
+    },
+    {
+        Header: "Rating",
+        accessor: "rating",
+        width: 110,
+    },
 ];

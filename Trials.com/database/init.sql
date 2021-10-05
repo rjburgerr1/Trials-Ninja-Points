60606107ca233ba6a6ce6a5edaaf79a01ec5172c
Randall (RJ) Bradach <49658348+rjburgerr1@users.noreply.github.com>
2021-10-04 23:32:38 -0500

    Update Database init cript, and env vars for prisma schema

--- Trials.com/database/init.sql
+++ Trials.com/database/init.sql
@@ -36,7 +36,7 @@ CREATE TABLE `creators` (
 
 LOCK TABLES `creators` WRITE;
 /*!40000 ALTER TABLE `creators` DISABLE KEYS */;
-INSERT INTO `creators` VALUES ('121'),('12123e'),('12312e'),('123qwd'),('123v12ve'),('12e'),('12ec12e'),('12ev12e'),('12v'),('12v312'),('12ve'),('23b23r'),('23rb'),('23rv23'),('2v23v2v'),('34b34'),('34tn34'),('43b'),('Classyfication'),('crasd'),('dqwdqw'),('ev12ev'),('lala'),('NEw'),('o12'),('od'),('p,p,p,21,p'),('pap'),('plpld21'),('qwd'),('qwdbqb'),('qwdq'),('qwdv'),('qwdvqwd'),('qwv'),('rar'),('RJ'),('rrrrrr'),('rrwer'),('rv'),('Slik'),('someone'),('t23n'),('v123v1'),('v12ev12ev'),('v1v1'),('v23rv23r'),('vqwd1'),('vqwev'),('wdvqwdv'),('wef'),('wefbwe'),('wefvwe1v'),('wefwe'),('wefwef'),('werwer'),('wevqwe'),('wqd');
+INSERT INTO `creators` VALUES ('`wqedv'),('121'),('12123e'),('12312e'),('123qwd'),('123v12ve'),('12e'),('12ec12e'),('12ev12e'),('12v'),('12v312'),('12ve'),('23b23r'),('23rb'),('23rv23'),('2v23v2v'),('34b34'),('34tn34'),('43b'),('b23b23r'),('b23rb23b2'),('Classyfication'),('crasd'),('dqwdqw'),('e12ev12'),('ev12ev'),('lala'),('NEw'),('o12'),('od'),('p,p,p,21,p'),('pap'),('plpld21'),('qwd'),('qwdbqb'),('qwdq'),('qwdv'),('qwdvqwd'),('qwv'),('rar'),('RJ'),('rrrrrr'),('rrwer'),('rv'),('Slik'),('someone'),('t23n'),('v123v1'),('v12ev1'),('v12ev12ev'),('v12ev12ev12ev1'),('v1v1'),('v23rv23r'),('vqwd1'),('vqwdvqwdvq'),('vqwev'),('wdvqwdv'),('wef'),('wefbwe'),('wefvwe1v'),('wefwe'),('wefwef'),('werwer'),('wevqwe'),('wqd');
 /*!40000 ALTER TABLE `creators` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -95,7 +95,7 @@ CREATE TABLE `riders` (
 
 LOCK TABLES `riders` WRITE;
 /*!40000 ALTER TABLE `riders` DISABLE KEYS */;
-INSERT INTO `riders` VALUES ('12312'),('12ev123'),('1v2v12'),('21ev12ev'),('21ve12evv12'),('c12ec'),('feewfbweb'),('person'),('qdqvqw'),('qqwdvqw'),('qvqwdvqwd'),('qvwdv'),('qvwdvq'),('qvwdvqw'),('qvwevqwev'),('qwdqvqwd'),('qwdv'),('qwdvqv'),('qwdvqwdv'),('qwvdvqwd'),('RJ Burgerr1'),('v123ev12e'),('v12e'),('v12ev12'),('v12ev12e'),('v12v12'),('vqwd'),('vqwdv'),('vqwv'),('wdqv'),('webwefb'),('wefvwef'),('wqvvqvq');
+INSERT INTO `riders` VALUES ('12312'),('12ev123'),('1v2v12'),('21ev12ev'),('21ve12evv12'),('c12ec'),('feewfbweb'),('LALALALAL'),('person'),('qdqvqw'),('qqwdvqw'),('qv 3rb32rb3r'),('qvqwdvqwd'),('qvwdv'),('qvwdvq'),('qvwdvqw'),('qvwdvqwdv'),('qvwevqwev'),('qwdqvqwd'),('qwdv'),('qwdvqv'),('qwdvqwdv'),('qwvdvqwd'),('r32b'),('RJ Burgerr1'),('RJRJRJ'),('v123ev12e'),('v12e'),('v12ev12'),('v12ev12e'),('v12v12'),('v12ve12ev'),('ve1v1vev1e'),('vqwd'),('vqwdv'),('vqwv'),('wdqv'),('webwefb'),('wefvwef'),('wqvvqvq');
 /*!40000 ALTER TABLE `riders` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -134,7 +134,7 @@ CREATE TABLE `runs` (
 
 LOCK TABLES `runs` WRITE;
 /*!40000 ALTER TABLE `runs` DISABLE KEYS */;
-INSERT INTO `runs` VALUES ('21ve12evv12',1234,124,'12:41.241','rar',327.00,6.00,'Medium','Not At All',2.00,'rar'),('feewfbweb',123,123,'12:31.231','dvqwdvq',195.00,4.00,'Short','Very',5.00,'wdvqwdv'),('qqwdvqw',12,123,'12:31.231','dvqwdvq',285.00,5.00,'Long','Not At All',2.00,'wdvqwdv'),('qvwdv',123,123,'31:31.231','qwdvqwdv',244.00,6.00,'Medium','Not Very',3.00,'123v12ve'),('qvwdvqw',12,123,'12:31.231','lala',319.00,5.00,'Short','Moderately',3.00,'lala'),('qvwevqwev',3,123,'12:31.231','qwevq',2363.03,9.00,'Long','Extremely',5.00,'wevqwe'),('qwdqvqwd',123,123,'12:31.231','vqwdvqwvq',239.00,5.00,'Short','Not Very',3.00,'12v312'),('qwdv',123,123,'12:31.231','pap',27.00,2.00,'Medium','Not Very',2.00,'pap'),('qwdvqv',2314,124,'12:41.241','rar',404.00,5.00,'Medium','Moderately',4.00,'rar'),('qwvdvqwd',1231,123,'12:31.231','vqwd',289.00,6.00,'Short','Not Very',3.00,'vqwd1'),('vqwd',123,123,'12:31.231','qwdv',406.00,5.00,'Medium','Moderately',3.00,'qwdvqwd'),('wqvvqvq',414,33,'12:31.231','dvqwdvq',185.00,4.00,'Medium','Not Very',3.00,'wdvqwdv');
+INSERT INTO `runs` VALUES ('21ve12evv12',1234,124,'12:41.241','rar',327.00,6.00,'Medium','Not At All',2.00,'rar'),('feewfbweb',123,123,'12:31.231','dvqwdvq',195.00,4.00,'Short','Very',5.00,'wdvqwdv'),('LALALALAL',124,142,'12:41.241','12evqwvq',356.97,5.50,'Medium','Not Very',3.00,'v12ev1'),('qqwdvqw',12,123,'12:31.231','dvqwdvq',285.00,5.00,'Long','Not At All',2.00,'wdvqwdv'),('qv 3rb32rb3r',2142,234,'23:42.342','b23rb23r',230.09,6.00,'Medium','Not At All',3.00,'b23rb23b2'),('qvwdv',123,123,'31:31.231','qwdvqwdv',244.00,6.00,'Medium','Not Very',3.00,'123v12ve'),('qvwdvqw',12,123,'12:31.231','lala',319.00,5.00,'Short','Moderately',3.00,'lala'),('qvwdvqwdv',21,123,'12:31.231','qwdvqwdvqwd',304.42,5.00,'Medium','Not Very',3.00,'vqwdvqwdvq'),('qvwevqwev',3,123,'12:31.231','qwevq',2363.03,9.00,'Long','Extremely',5.00,'wevqwe'),('qwdqvqwd',123,123,'12:31.231','vqwdvqwvq',239.00,5.00,'Short','Not Very',3.00,'12v312'),('qwdv',123,123,'12:31.231','pap',27.00,2.00,'Medium','Not Very',2.00,'pap'),('qwdvqv',2314,124,'12:41.241','rar',404.00,5.00,'Medium','Moderately',4.00,'rar'),('qwvdvqwd',1231,123,'12:31.231','vqwd',289.00,6.00,'Short','Not Very',3.00,'vqwd1'),('r32b',2323,232,'23:23.232','b23rb',34.40,2.00,'Medium','Not Very',3.00,'b23b23r'),('RJRJRJ',123,123,'12:31.231','adww',371.33,4.50,'Short','Extremely',3.00,'`wqedv'),('v12ve12ev',2,123,'23:12.312','12ev12ve12e',279.27,6.00,'Short','Not Very',3.00,'v12ev12ev12ev1'),('ve1v1vev1e',121212,123,'41:24.141','v12ev12e1',160.65,4.50,'Medium','Moderately',3.00,'e12ev12'),('vqwd',123,123,'12:31.231','qwdv',406.00,5.00,'Medium','Moderately',3.00,'qwdvqwd'),('wqvvqvq',414,33,'12:31.231','dvqwdvq',185.00,4.00,'Medium','Not Very',3.00,'wdvqwdv');
 /*!40000 ALTER TABLE `runs` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -169,7 +169,7 @@ CREATE TABLE `tracks` (
 
 LOCK TABLES `tracks` WRITE;
 /*!40000 ALTER TABLE `tracks` DISABLE KEYS */;
-INSERT INTO `tracks` VALUES ('dvqwdvq','wdvqwdv',4.00,2.00,93.00,2.00,3.0,3,10.00,7.00,6.00,279,13.00),('lala','lala',5.00,1.00,123.00,3.00,3.0,1,3.00,0.00,0.00,0,0.00),('pap','pap',2.00,2.00,123.00,2.00,2.0,1,2.00,0.00,0.00,0,0.00),('qwdv','qwdvqwd',5.00,2.00,123.00,3.00,3.0,1,3.00,0.00,0.00,0,0.00),('qwdvqwdv','123v12ve',6.00,2.00,123.00,2.00,3.0,1,3.00,0.00,0.00,0,0.00),('qwevq','wevqwe',NULL,NULL,NULL,NULL,NULL,1,0.00,0.00,0.00,0,0.00),('rar','rar',6.00,2.00,124.00,2.00,3.0,2,6.00,0.00,0.00,0,0.00),('vqwd','vqwd1',6.00,1.00,123.00,2.00,3.0,0,0.00,0.00,0.00,0,0.00),('vqwdvqwvq','12v312',5.00,1.00,123.00,2.00,3.0,0,0.00,0.00,0.00,0,0.00);
+INSERT INTO `tracks` VALUES ('12ev12ve12e','v12ev12ev12ev1',6.00,1.00,123.00,2.00,3.0,1,3.00,2.00,1.00,123,6.00),('12evqwvq','v12ev1',5.50,2.00,142.00,2.00,3.0,1,3.00,2.00,2.00,142,5.50),('adww','`wqedv',4.50,1.00,123.00,5.00,3.0,1,3.00,5.00,1.00,123,4.50),('b23rb','b23b23r',2.00,2.00,232.00,2.00,3.0,1,3.00,2.00,2.00,232,2.00),('b23rb23r','b23rb23b2',6.00,2.00,234.00,1.00,3.0,1,3.00,1.00,2.00,234,6.00),('dvqwdvq','wdvqwdv',4.33,2.00,93.00,2.33,3.3,3,10.00,7.00,6.00,279,13.00),('lala','lala',5.00,1.00,123.00,3.00,3.0,1,3.00,0.00,0.00,0,0.00),('pap','pap',2.00,2.00,123.00,2.00,2.0,1,2.00,0.00,0.00,0,0.00),('qwdv','qwdvqwd',5.00,2.00,123.00,3.00,3.0,1,3.00,0.00,0.00,0,0.00),('qwdvqwdv','123v12ve',6.00,2.00,123.00,2.00,3.0,1,3.00,0.00,0.00,0,0.00),('qwdvqwdvqwd','vqwdvqwdvq',5.00,2.00,123.00,2.00,3.0,0,0.00,0.00,0.00,0,0.00),('qwevq','wevqwe',9.00,3.00,123.00,5.00,5.0,1,0.00,0.00,0.00,0,0.00),('rar','rar',5.50,2.00,124.00,2.00,3.0,2,6.00,0.00,0.00,0,0.00),('v12ev12e1','e12ev12',4.50,2.00,123.00,3.00,3.0,1,3.00,3.00,2.00,123,4.50),('vqwd','vqwd1',6.00,1.00,123.00,2.00,3.0,0,0.00,0.00,0.00,0,0.00),('vqwdvqwvq','12v312',5.00,1.00,123.00,2.00,3.0,0,0.00,0.00,0.00,0,0.00);
 /*!40000 ALTER TABLE `tracks` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -214,4 +214,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-10-03 19:40:50
+-- Dump completed on 2021-10-04 23:26:58

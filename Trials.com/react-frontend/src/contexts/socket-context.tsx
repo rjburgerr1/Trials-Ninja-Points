9bfec72f819c376029f70538f66eb6ad1ff2a8e1
Randall (RJ) Bradach <49658348+rjburgerr1@users.noreply.github.com>
2021-10-05 02:44:48 -0500

    Testing production build

--- Trials.com/react-frontend/src/contexts/socket-context.tsx
+++ Trials.com/react-frontend/src/contexts/socket-context.tsx
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 import React from "react";
 
-export const socket = io(process.env.REACT_APP_AXIOS_BASE_URL || "", {
+export const socket = io(process.env.REACT_APP_AXIOS_BASE_URL_PROD || "", {
     transports: ["websocket"],
 });
 export const SocketProvider = React.createContext<any>({} as any);

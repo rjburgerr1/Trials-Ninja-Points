3043ac3e611dba4c2d15fe55293e4ec187596c00
Randall (RJ) Bradach <49658348+rjburgerr1@users.noreply.github.com>
2022-01-28 13:18:08 -0600

    Remove unnecessary import

--- Trials.com/react-frontend/src/components/helpers/private-route.tsx
+++ Trials.com/react-frontend/src/components/helpers/private-route.tsx
@@ -1,6 +1,6 @@
 // PrivateRoute.tsx in v6
 
-import { Navigate, Route, useLocation } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 import { useAuth } from "../../contexts/auth-context";
 
 const PrivateRoute = ({ children }: { children: JSX.Element }) => {

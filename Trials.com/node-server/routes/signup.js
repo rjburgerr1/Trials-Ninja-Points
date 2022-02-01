c16ecdc0390e0ec262086f680ae0f65687170949
Randall (RJ) Bradach <49658348+rjburgerr1@users.noreply.github.com>
2022-02-01 15:38:02 -0600

    Change signup order
    - Check if username exists before creating account on firebase
    -- proceed with signing up for firebase
    -- next. add user to database
    - Add error message when failed signup

--- Trials.com/node-server/routes/signup.js
+++ Trials.com/node-server/routes/signup.js
@@ -4,13 +4,33 @@ const prisma = new PrismaClient();
 const router = (app) => {
     // Add a profile to the databasewhen signup is successful
     app.post("/sign-up-complete", async (request, response) => {
-        await prisma.profiles.create({
-            data: {
-                email: request.body.email,
-                username: request.body.username,
-                id: request.body.userid,
-            },
-        });
+        try {
+            await prisma.profiles.create({
+                data: {
+                    email: request.body.email,
+                    username: request.body.username,
+                    id: request.body.userid,
+                },
+            });
+            return response.sendStatus(200);
+        } catch (error) {
+            console.log(error);
+            return response.status(400).send(error);
+        }
+    });
+
+    app.get("/does-username-exist", async (request, response) => {
+        try {
+            await prisma.profiles.findFirst({
+                where: {
+                    username: request.query.username,
+                },
+            });
+            return response.sendStatus(200);
+        } catch (error) {
+            console.log(error);
+            return response.status(400).send(error);
+        }
     });
 };
 // Export the router
--- Trials.com/react-frontend/src/components/auth/signup.tsx
+++ Trials.com/react-frontend/src/components/auth/signup.tsx
@@ -2,85 +2,130 @@ import React, { useRef, useState } from "react";
 import { useAuth } from "../../contexts/auth-context";
 import { Alert } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
-import signupPrisma from "./prisma-signup";
+import { signupPrisma, checkIfUserExists } from "./prisma-signup";
+import * as Yup from "yup";
+import { Field, Form, Formik } from "formik";
+import { FieldError } from "../helpers/field-error";
+
+interface FormValues {
+    username: string;
+    email: string;
+    password: string;
+}
 
 const SignupComponent = () => {
-    const emailRef = useRef<HTMLInputElement>(null);
-    const passwordRef = useRef<HTMLInputElement>(null);
-    const usernameRef = useRef<HTMLInputElement>(null);
-
     const { signup } = useAuth();
-    const [error, setError] = useState("");
+    const [error, setError] = useState(null);
     const [loading, setLoading] = useState(false);
     const navigate = useNavigate();
 
-    const handleSubmit = async (event: any) => {
-        event.preventDefault();
-
+    const handleSubmit = async (values: any) => {
         try {
-            setError("");
             setLoading(true);
+            // Check if a user exists with the same username before creating
+            // account on firebase to get uid to then add user to DB
+            await checkIfUserExists(values.username);
 
-            if (
-                emailRef.current != null &&
-                usernameRef.current != null &&
-                passwordRef.current != null
-            ) {
-                const response = await signup(
-                    emailRef.current.value,
-                    passwordRef.current.value,
-                    usernameRef.current.value
-                );
-                signupPrisma(
-                    emailRef.current.value,
-                    usernameRef.current.value,
-                    response.user.uid
-                );
-            }
+            const response = await signup(
+                values.email,
+                values.password,
+                values.username
+            );
+
+            await signupPrisma(
+                values.email,
+                values.username,
+                response.user.uid
+            );
 
             navigate("/");
         } catch (error: any) {
-            console.log(error);
-            setError(error.message);
+            console.log(error.message || error);
+            setError(error.message || error);
         }
-
         setLoading(false);
     };
+
+    const initialValues: FormValues = {
+        username: "",
+        email: "",
+        password: "",
+    };
+
+    const SignupSchema = Yup.object({
+        username: Yup.string()
+            .max(25, "Username is too long")
+            .required("Username is Required"),
+        email: Yup.string()
+            .email("Must be a valid email")
+            .max(255)
+            .required("Email is required"),
+
+        password: Yup.string()
+            .min(6, "Password is too short")
+            .required("Required"),
+    });
 
     return (
         <div className="form-container sign-up-container">
             {error && <Alert variant="danger">{error}</Alert>}
 
-            <form onSubmit={handleSubmit}>
-                <h1 id="create-account-header">Create Account</h1>
+            <Formik
+                initialValues={initialValues}
+                validationSchema={SignupSchema}
+                onSubmit={handleSubmit}
+            >
+                {(props: any) => (
+                    <Form>
+                        <h1 id="create-account-header">Create Account</h1>
+                        <div className="form-inline-group">
+                            <div className="social-container"></div>
+                            <FieldError
+                                error={props.errors.username}
+                                touched={props.touched.username}
+                            />
+                            <Field
+                                id="username"
+                                name="username"
+                                onChange={props.handleChange}
+                                placeholder="Username"
+                                type="text"
+                                value={props.values.username}
+                            />
 
-                <div className="social-container"></div>
-                <input
-                    type="text"
-                    placeholder="Username"
-                    name="username"
-                    ref={usernameRef}
-                    autoComplete="false"
-                />
-                <input
-                    type="email"
-                    placeholder="Email"
-                    name="email"
-                    ref={emailRef}
-                    autoComplete="false"
-                />
-                <input
-                    type="password"
-                    placeholder="Password"
-                    name="password"
-                    ref={passwordRef}
-                    autoComplete="new-password"
-                />
+                            <FieldError
+                                error={props.errors.email}
+                                touched={props.touched.email}
+                            />
+                            <Field
+                                id="email"
+                                type="email"
+                                placeholder="Email"
+                                name="email"
+                                value={props.values.email}
+                                autoComplete="false"
+                            />
 
-                <button disabled={loading} type="submit">
-                    Sign Up
-                </button>
-            </form>
+                            <FieldError
+                                error={props.errors.password}
+                                touched={props.touched.password}
+                            />
+                            <Field
+                                id="password"
+                                type="password"
+                                placeholder="Password"
+                                name="password"
+                                value={props.values.password}
+                                autoComplete="new-password"
+                            />
+                        </div>
+
+                        <button disabled={loading} type="submit">
+                            Sign Up
+                        </button>
+                    </Form>
+                )}
+            </Formik>
         </div>
     );
 };

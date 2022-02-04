6392afbfc203eec3e18b0f3e8bddc4de1bc521bd
Randall (RJ) Bradach <49658348+rjburgerr1@users.noreply.github.com>
2022-02-04 13:01:09 -0600

    Remove unused variable

--- Trials.com/react-frontend/src/components/data-entry/text-inputs.tsx
+++ Trials.com/react-frontend/src/components/data-entry/text-inputs.tsx
@@ -53,7 +53,7 @@ export const CustomTextField = ({ label, innerRef, ...props }: any) => {
 export const CustomSelect = ({ label, ...props }: any) => {
     // useField() returns [formik.getFieldProps(), formik.getFieldMeta()]
     // which we can spread on <input> and alse replace ErrorMessage entirely.
-    const [field, meta] = useField(props);
+    const [field] = useField(props);
     return (
         <>
             <label

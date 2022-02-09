d6ccd2c739c626790e6b2a53420830f0a9f7ce4b
Randall (RJ) Bradach <49658348+rjburgerr1@users.noreply.github.com>
2022-02-09 02:08:33 -0600

    Add try/catches

--- Trials.com/node-server/synthesize-run-data/get-highest-pass.js
+++ Trials.com/node-server/synthesize-run-data/get-highest-pass.js
@@ -1,18 +1,22 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 const getHighestLevelPass = async (riderID) => {
-    const highestLevelPasses = await prisma.runs.aggregate({
-        where: {
-            id: riderID,
-        },
-        orderBy: {
-            ninja_points: "desc",
-        },
-        _max: {
-            ninja_level: true,
-        },
-    });
+    try {
+        const highestLevelPasses = await prisma.runs.aggregate({
+            where: {
+                id: riderID,
+            },
+            orderBy: {
+                ninja_points: "desc",
+            },
+            _max: {
+                ninja_level: true,
+            },
+        });
 
-    return highestLevelPasses._max.ninja_level;
+        return highestLevelPasses._max.ninja_level;
+    } catch (error) {
+        console.log(error);
+    }
 };
 module.exports = getHighestLevelPass;

698c4edc18a460fca84ae73e5242102d230ac071
Randall (RJ) Bradach <49658348+rjburgerr1@users.noreply.github.com>
2022-02-18 12:44:53 -0600

    Change likert scales to reflect correct weights

--- Trials.com/react-frontend/src/components/helpers/convert-fields.tsx
+++ Trials.com/react-frontend/src/components/helpers/convert-fields.tsx
@@ -1,7 +1,7 @@
 export const convertLength = (length: number) => {
-    if (length <= 1.5) {
+    if (length <= 1.1) {
         return "Short";
-    } else if (length > 1.5 && length <= 2.5) {
+    } else if (length > 1.1 && length <= 1.4) {
         return "Medium";
     } else {
         return "Long";
@@ -9,13 +9,13 @@ export const convertLength = (length: number) => {
 };
 
 export const convertConsistency = (consistency: number) => {
-    if (consistency <= 1.5) {
+    if (consistency <= 0.9) {
         return "Not At All";
-    } else if (consistency > 1.5 && consistency <= 2.5) {
+    } else if (consistency > 0.9 && consistency <= 1.2) {
         return "Not Very";
-    } else if (consistency > 2.5 && consistency <= 3.5) {
+    } else if (consistency > 1.2 && consistency <= 1.6) {
         return "Moderately";
-    } else if (consistency > 3.5 && consistency <= 4.5) {
+    } else if (consistency > 1.6 && consistency <= 2) {
         return "Very";
     } else {
         return "Extremely";

3790edaed517ee6a52f7d6bfc98867a8f130dd18
Randall (RJ) Bradach <49658348+rjburgerr1@users.noreply.github.com>
2022-03-10 14:08:12 -0600

    Create generic page template for most pages

---
+++ Trials.com/react-frontend/src/pages/generic-page.tsx
@@ -0,0 +1,22 @@
+import NavBar from "../components/helpers/navbar";
+
+/**
+ * This is our component for generic pages
+ * The layout for generic pages are as follows:
+ * One Navigation bar header + One component for the body
+ *
+ * If we need a more complicated layout, create a separate page for the component
+ *
+ * @param props
+ * @returns
+ */
+export default function GenericPage(props: any) {
+    return (
+        <div className="page-container">
+            <div className="page-header">
+                <NavBar {...props} />
+            </div>
+            <div className="page-body">{props.component}</div>
+        </div>
+    );
+}

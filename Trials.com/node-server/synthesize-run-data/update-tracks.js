a370cb7af2e7e92251032957187e0889e58638b0
Randall (RJ) Bradach <49658348+rjburgerr1@users.noreply.github.com>
2022-03-14 17:08:42 -0500

    Fix track table updates
    - Add deletion check to delete any track that no longer has any runs connected to it, but continues to persist in the tracks table
    - Add adequate try/catches
    - Update init.sql - remove one stale row

--- Trials.com/node-server/synthesize-run-data/update-tracks.js
+++ Trials.com/node-server/synthesize-run-data/update-tracks.js
@@ -1,6 +1,5 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
-const synthesizeData = require("../synthesize-run-data/synthesize-data");
 
 /**
  * This is a dangerous update. It mutates values. We should change this to not mutate values
@@ -49,69 +48,93 @@ const updateTrackAvgs = async (tracks) => {
     // Average Data for tracks again
 
     for (const track of tracks) {
-        const avgConsistency = track.total_consistency / track.nRuns;
-        const avgLength = track.total_length / track.nRuns;
-        const avgFaults = track.total_faults / track.nRuns;
-        const avgNinjaLevel = track.total_ninja_level / track.nRuns;
-        const avgNP = track.total_np / track.nRuns;
+        try {
+            if (track.nRuns !== 0) {
+                const avgConsistency = track.total_consistency / track.nRuns;
+                const avgLength = track.total_length / track.nRuns;
+                const avgFaults = track.total_faults / track.nRuns;
+                const avgNinjaLevel = track.total_ninja_level / track.nRuns;
+                const avgNP = track.total_np / track.nRuns;
 
-        await prisma.tracks.update({
-            where: {
-                track_name_creator: {
-                    track_name: track.track_name,
-                    creator: track.creator,
-                },
-            },
-            data: {
-                consistency: avgConsistency,
-                length: avgLength,
-                average_faults: avgFaults,
-                ninja_level: avgNinjaLevel,
-                average_np: avgNP,
-            },
-        });
+                await prisma.tracks.update({
+                    where: {
+                        track_name_creator: {
+                            track_name: track.track_name,
+                            creator: track.creator,
+                        },
+                    },
+                    data: {
+                        consistency: avgConsistency,
+                        length: avgLength,
+                        average_faults: avgFaults,
+                        ninja_level: avgNinjaLevel,
+                        average_np: avgNP,
+                    },
+                });
+            } else {
+                await prisma.tracks.delete({
+                    where: {
+                        track_name_creator: {
+                            track_name: track.track_name,
+                            creator: track.creator,
+                        },
+                    },
+                });
+            }
+        } catch (error) {
+            console.error(error);
+        }
     }
 };
 
 const updateTrackTotals = async (runs) => {
-    await prisma.tracks.updateMany({
-        data: {
-            total_consistency: 0,
-            total_length: 0,
-            total_faults: 0,
-            total_ninja_level: 0,
-            total_np: 0,
-        },
-    });
-    for (const run of runs) {
-        const consistency = calcConsistency(run.consistency);
-        const length = calcLength(run.length);
-
-        const track = await prisma.tracks.update({
-            where: {
-                track_name_creator: {
-                    track_name: run.track_name,
-                    creator: run.creator,
-                },
-            },
+    try {
+        await prisma.tracks.updateMany({
             data: {
-                total_consistency: {
-                    increment: consistency,
-                },
-                total_length: {
-                    increment: length,
-                },
-                total_faults: {
-                    increment: run.faults,
-                },
-                total_ninja_level: {
-                    increment: run.ninja_level,
-                },
-                total_np: {
-                    increment: run.ninja_points,
-                },
+                total_consistency: 0,
+                total_length: 0,
+                total_faults: 0,
+                total_ninja_level: 0,
+                total_np: 0,
             },
         });
+    } catch (error) {
+        console.error(error);
+    }
+
+    for (const run of runs) {
+        try {
+            const consistency = calcConsistency(run.consistency);
+            const length = calcLength(run.length);
+
+            await prisma.tracks.update({
+                where: {
+                    track_name_creator: {
+                        track_name: run.track_name,
+                        creator: run.creator,
+                    },
+                },
+                data: {
+                    total_consistency: {
+                        increment: consistency,
+                    },
+                    total_length: {
+                        increment: length,
+                    },
+                    total_faults: {
+                        increment: run.faults,
+                    },
+                    total_ninja_level: {
+                        increment: run.ninja_level,
+                    },
+                    total_np: {
+                        increment: run.ninja_points,
+                    },
+                },
+            });
+        } catch (error) {
+            console.error(error);
+        }
     }
     return true;
 };
